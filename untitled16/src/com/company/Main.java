4c1132e2ddb3e9eeed09c126a97232d1088d2cc4
Ali <110012339+sherali2007@users.noreply.github.com>
2022-07-30 03:08:54 -0400

    Add files via upload

---
+++ untitled16/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a = 5, b = 6, c = 4, d = 7;
+
+        if (a > 0 && b > 0 && c > 0 && d > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
